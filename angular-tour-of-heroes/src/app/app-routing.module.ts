3cf80008a1296bfe5051046981f2dc0fb2dbba5a
Unknown <shinchri7@gmail.com>
2018-04-04 14:30:46 -0500

    configured the router at root level

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -7,6 +7,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
 })
 export class AppRoutingModule {}
